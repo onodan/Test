515c74f7bbab01c1aa736885d5f8f2be440c4231
onodan <45358532+onodan@users.noreply.github.com>
2021-09-03 10:58:45 +0700

    Add files via upload

---
+++ code/page-about.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Template Name: О нас
+*/
+?>
+
+<?php $page_id = get_the_ID(); ?>
+<?php get_header();?>
+
+<section class="section single-page">
+<div class="container single-page-container">
+<h1 class="page-title"><?php the_title();?></h1>
+<?php if ( have_posts() ) : ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+    <div class="single-page-content">
+    <?php the_content();?>
+
+    </div>
+       <?php endwhile; ?>
+
+<?php endif; ?>
+</div>
+</section>
+
+
+<?php get_footer();?>
